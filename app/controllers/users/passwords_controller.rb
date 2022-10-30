23644d60750d8b19dd7606337d79459abb10d519
Sarah Branch <sarah@Sarahs-MacBook-Pro-8.local>
2022-10-30 15:24:01 -0400

    adding user controller

---
+++ app/controllers/users/passwords_controller.rb
@@ -0,0 +1,49 @@
+# frozen_string_literal: true
+
+class Users::PasswordsController < Devise::PasswordsController
+  # GET /resource/password/new
+  # def new
+  #   super
+  # end
+
+  # POST /resource/password
+  # def create
+  #   super
+  # end
+
+  # GET /resource/password/edit?reset_password_token=abcdef
+  # def edit
+  #   super
+  # end
+
+  # PUT /resource/password
+  # def update
+  #   super
+  # end
+
+  # protected
+
+  # def after_resetting_password_path_for(resource)
+  #   super(resource)
+  # end
+
+  # The path used after sending reset password instructions
+  # def after_sending_reset_password_instructions_path_for(resource_name)
+  #   super(resource_name)
+  # end
+
+
+  # <% if flash[:notice] %>
+  #       <p class="alert"><%= flash[:notice] %></p>
+  # <% end %>
+  # if flash[:alert] 
+  #       <%#  flash[:alert] 
+  #       <p class="alert"><%= flash[:not_found_in_database] 
+  #   end 
+
+    # if user_not_found_in_database?
+    #   flash[:alert] = "hello"
+    # end
+
+
+end
