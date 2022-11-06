047b36170dead55164c8bb07c183408ce1bc6ae4
ashruman <auman@smith.edu>
2022-11-06 16:27:21 -0500

    reverted login to email-only and added account page and user balance

--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -3,13 +3,10 @@
 class Users::SessionsController < Devise::SessionsController
   # before_action :configure_sign_in_params, only: [:create]
 
-  GET /resource/sign_in
-  def new
-    super
-    if user_not_found_in_database?
-        flash[:alert] = "hello"
-      end
-  end
+  # GET /resource/sign_in
+  # def new
+  #   super
+  # end
 
   # POST /resource/sign_in
   # def create
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,5 +2,7 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+         :recoverable, :rememberable, :validatable,
+         :authentication_keys => [:email] #gives reg error when email not present
+  validates :email, uniqueness: true
 end
