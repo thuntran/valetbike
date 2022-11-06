e2d67b262aa772f8c5fbf37db8bb10988154bf2c
ashruman <auman@smith.edu>
2022-11-06 16:27:21 -0500

    reverted login to email-only and added account page and user balance

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_10_26_172818) do
+ActiveRecord::Schema[7.0].define(version: 2022_11_06_205955) do
   create_table "bikes", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
     t.integer "identifier"
     t.integer "current_station_id"
@@ -35,6 +35,8 @@ ActiveRecord::Schema[7.0].define(version: 2022_10_26_172818) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "username"
+    t.float "col1"
+    t.float "balance"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
