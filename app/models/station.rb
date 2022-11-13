2fa41d956c064f961a932e4bef5c06550f67dae2
Thu Tran <tntran@smith.edu>
2022-11-12 20:30:25 -0500

    Set up csv import for station list

--- app/models/station.rb
+++ app/models/station.rb
@@ -1,7 +1,10 @@
 class Station < ApplicationRecord
   validates_presence_of    :identifier,
                            :name,
-                           :address
+                           :address,
+                           :dock_count
+                           :docked_bike_count
+
   validates_uniqueness_of  :identifier
   
   has_many :docked_bikes, class_name: :Bike, foreign_key: :current_station_id
