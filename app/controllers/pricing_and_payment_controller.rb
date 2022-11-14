8acebe56140e6af68191948e593e66f8e4e3d4c6
Sarah Branch <sarah@Sarahs-MacBook-Pro-8.local>
2022-11-13 19:40:00 -0500

    add changes to makeit so user can pick a station and pick a bike at that station

--- app/controllers/pricing_and_payment_controller.rb
+++ app/controllers/pricing_and_payment_controller.rb
@@ -1,9 +1,21 @@
 class PricingAndPaymentController < ApplicationController
   def index
     @stations = Station.all.order(identifier: :asc)
+    @bikes = Bike.all.order(identifier: :asc)
 
-    if params[:selected_station]
-      @bikes = Bike.all.order(identifier: :asc)
+    if params[:station]
+      @chosenStation = Station.find_by(address: params[:station])
+      @theseBikes = @chosenStation.docked_bikes
     end
+
+    if params[:bike]
+      @chosenBike = Bike.find_by(identifier: params[:bike])
+      @chosenStation = @chosenBike.current_station
+      @theseBikes = @chosenStation.docked_bikes
+    end
+  end
+
+  def ride
+    #@bike = Bike.find_by(identifier: params[:bike])
   end
 end
--- app/models/bike.rb
+++ app/models/bike.rb
@@ -6,5 +6,6 @@ class Bike < ApplicationRecord
   validates_uniqueness_of  :identifier
   
   belongs_to :current_station, class_name: :Station, foreign_key: :current_station_id, optional: true
+  belongs_to :current_user, class_name: :User, foreign_key: :current_user_id, optional: true
 
 end
