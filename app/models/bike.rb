c70e0214e148cb342382ee372b5c226d9bd11852
Thu Tran <tntran@smith.edu>
2022-11-15 15:46:07 -0500

    Add bike percentage change to csv, update station names, add hyperlinks to station locations, experiment on styling submit buttons

--- app/models/bike.rb
+++ app/models/bike.rb
@@ -1,5 +1,8 @@
 class Bike < ApplicationRecord
-  validates_presence_of    :identifier
+  validates_presence_of    :identifier,
+                           :current_station_id,
+                           :battery_percentage
+                           
   validates_uniqueness_of  :identifier
   
   belongs_to :current_station, class_name: :Station, foreign_key: :current_station_id, optional: true
--- db/migrate/20220831090719_create_bikes.rb
+++ db/migrate/20220831090719_create_bikes.rb
@@ -3,6 +3,7 @@ class CreateBikes < ActiveRecord::Migration[7.0]
     create_table :bikes do |t|
       t.integer :identifier
       t.integer :current_station_id
+      t.integer :battery_percentage
 
       t.timestamps
     end
