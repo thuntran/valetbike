419bbc775f374dbb04a0afbd7d77f4bad56c32cb
Thu Tran <tntran@smith.edu>
2022-12-01 15:16:24 -0500

    Small button change

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_11_16_192954) do
+ActiveRecord::Schema[7.0].define(version: 2022_11_18_210113) do
   create_table "bikes", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
     t.integer "identifier"
     t.integer "current_station"
