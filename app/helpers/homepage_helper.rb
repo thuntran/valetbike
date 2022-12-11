b390b2ce60edaa69f2b5466f896009315ad22c9b
Sarah Branch <sarah@Sarahs-MacBook-Pro-8.local>
2022-12-11 01:52:20 -0500

    added border to forms, and added jerry

--- app/helpers/homepage_helper.rb
+++ app/helpers/homepage_helper.rb
@@ -1,6 +1,61 @@
 module HomepageHelper
     def ShowPuns! 
-        def puns = ["What does a mountain biker do when he wants to achieve more balance? He hires a cycleologist.","My cousin bought a bike and named it “The Truth”. I told him to stay away from the bike cus he kept on falling. I guess he couldn't handle The Truth.","I saw a chap painting pictures of bikes on a local church roof. His name is Cycleangelo.","My bike started to fall apart as soon as the chain broke, you could say it was a chain reaction.","I illegally cut down some trees when I was out building a trail. Nobody witnessed it, but my chain saw.","I avoid bike trails after dark. They're full of cycle paths.","What did the dirt bike say to the puddle? 'Are you my mudder?'","On this day last year, my wife got me a stationary bike for my birthday. Well, it's a regular bike, but it hasn't moved in 364 days.","I rode my bike so much, I had to put a new set of wheels on it. I was gonna put a third set on it, but the ol' bike just couldn't do the job anymore. It's understandable. The bike was already re-tired.","I bought an exercise bike, but I got rid of it after 2 weeks.. Didn't seem to be getting anywhere.","My mate punched a driver for pulling into the bike lane. He's a bit of a cycle-path.","My bike always looks at me with a sense of sexual resentment. I think it wants to ride on top for a while.","A maniac cut someone in half while I was on my bike today. I missed it, by my chain saw.","My cousin loves e-bikes because she's really indecisive. She likes that it takes charge.","I used some paper to make a bike. It just doesn't move ; it's a stationery bike.","Can you handle my bike jokes? Or do you need a brake?","What's a military bike team called? Cycle-ops.","What did the person say to the biker who was taking a long time to fix his bike? Aren't you tire-d yet?","My bike is mean to me. It's a vicious cycle.","I used to be obsessed with my bike, going out three or four times a day. But now, I've managed to break the cycle.","My bank manager has finally given up on riding his bike. He's lost his balance.","I was far quicker on my bike today than yesterday. I was in a totally different gear.","I was selling my bike and an interested buyer asked what's the lowest I'd go. About 2mph I said, otherwise you'd tip over.","I returned my new bike to the shop and explained that the pedals weren't working. The owner said, “that's why it's called a push bike.”","I crossed a bike with a flower and got… cycle petals.","A woodcutter built his own motorbike and used wood for the frame, the engine and even the brakes. But it wooden start.","What's the difference between a well-dressed man on a unicycle, and a poorly dressed man on a bike? A-ttire.","What do you call a bike trail that shows no empathy? A cycle path","So the cops just came to my door, they said my dog was chasing someone on a bike, I told them “that's ridiculous, my dog doesn't even own a bike”","I gave my bike a new name after it came back from the bell repair shop. It has a nice ring to it.","My teenage daughter was sitting idle on our spinning bike. I told her she needed to listen to less CardiB and start doing more CardiO.","I used to know a little boy who took his bike to bed. He didn't want to sleep walk.","I nearly ran over an old lady while on my bike yesterday. You need to learn to use a bell, she said. I know how to use a bell… I just can't ride my bike.","I spotted a toddler with cheese strapped to his trike. It must have been his baby bell.","My dog, Rover, used to chase everyone on a bike. I had to take his bike away.","Do you know what the hardest thing is about learning to ride a bike? The road.","I lycra your new bike.","I broke my bike today so I'll have to fork out for a new one.","When my bike hurts me, I kick it back. You could say we're in a vicious cycle.","Did you know Alfred Hitchcock used to be into downhill mountain biking? He was the master of suspens-ion.","A nostalgic mate's bike broke the same day he f*cked up a mixtape he was making for his girlfriend. Now, he needs a new cassette.","I rode my bike 10 miles to safely dispose of some paper, cans and bottles earlier. I was tired on the way back, I had to recycle.","My bike is absolutely disgusting these days. You should see the skidmarks.","I can't stand it when my bike keeps falling over.","I think y'all need to get a grip.","Northeast English joke: If you walk to Walker and bike to Byker, what do you do at Wallsend? You fall off.","I yelled “COW” at a woman on a bike. She flipped me off, then hit the cow.","Bought a stationary bike today, but I don't see myself getting anywhere with it.","Where do crazy people ride their bikes? On the psycho path.","An Italian man loses his hands from a bike accident. What did his parents ask the doctor? “Will my son be able to speak again?”","“Have you ever done anything good?” St. Peter asked a guy when he showed up at the pearly gates. “To protect a young girl, I punched the leader of a motorcycle gang, kicked his bike over, and told them all to back off!” said the man. St. Peter was impressed, “When did you do this?”. “Oh, just a couple of minutes ago.”","I am trying to teach my son how to put the chain back on his bike, but he still can't seem to get it. I guess it must be sprocket science.","My friend got into a squabble with a driver for swerving into the bike lane. He's a bit of a cyclepath.","I know a bike mechanic who is a bit scary, I guess he has always been a crank-y dude.","A cyclepath cut my bike in half. I missed it, but my chainsaw.","What does a biker do when he notices he's on the wrong trail? He back-pedals.","What's the perfect name for a sculptor who uses bike parts for his art. Cycleangelo.","There was a bike that caught my fancy, until I saw her skidmarks.","A barber won a bike race, I asked him how? He said he took a short cut.","What name did the chemist give to the bike he built? Bike-carbonate of Soda.","What do you call a bike that loves learning? A uni-cycle.","My bike-mad cousin dropped out of university when he realized he signed up for psychology."
+        def puns = ["What does a mountain biker do when he wants to achieve more balance? He hires a cycleologist.",
+            "My cousin bought a bike and named it “The Truth”. I told him to stay away from the bike cus he kept on falling. I guess he couldn't handle The Truth.",
+            "I saw a chap painting pictures of bikes on a local church roof. His name is Cycleangelo.",
+            "My bike started to fall apart as soon as the chain broke, you could say it was a chain reaction.",
+            "Shrimp fried rice. So you mean to tell me a shrimp fried this rice?", # from my friend
+            "I avoid bike trails after dark. They're full of cycle paths.",
+            "Wood fired pizza. How's pizza gonnaget a job now? ", # from my friend
+            "I can't ride a bike, don't trust myself to drive, and hate walking long distances. So clearly I should never leave my room.", # why not
+            "I rode my bike so much, I had to put a new set of wheels on it. I was gonna put a third set on it, but the ol' bike just couldn't do the job anymore. It's understandable. The bike was already re-tired.", 
+            "I bought an exercise bike, but I got rid of it after 2 weeks.. Didn't seem to be getting anywhere.",
+            "My mate punched a driver for pulling into the bike lane. He's a bit of a cycle-path.",
+            "I Googled how to ride a bike and the first video I saw was 'How to Ride a Bike from Scratch'. So I'm going to assume they baked a bike.", # why not
+            "Are you sure you're okay? :( okay with me dragging deez nuts across your face. ", # from my friend
+            "Please follow us on social media and give us a ValetLike!", # from asher
+            "Your mom.", # because i can
+            "Can you handle my bike jokes? Or do you need a brake?",
+            "What's a military bike team called? Cycle-ops.",
+            "What did the person say to the biker who was taking a long time to fix his bike? Aren't you tire-d yet?",
+            "My bike is mean to me. It's a vicious cycle.",
+            "What do ValetBike and ValleyBike have in common? I can't use either.", # why not
+            "My bank manager has finally given up on riding his bike. He's lost his balance.",
+            "I was far quicker on my bike today than yesterday. I was in a totally different gear.",
+            "I was selling my bike and an interested buyer asked what's the lowest I'd go. About 2mph I said, otherwise you'd tip over.",
+            "I returned my new bike to the shop and explained that the pedals weren't working. The owner said, “that's why it's called a push bike.”",
+            "I crossed a bike with a flower and got… cycle petals.",
+            "Our website is wheel-tastic! ", # why not 
+            "What's the difference between a well-dressed man on a unicycle, and a poorly dressed man on a bike? A-ttire.",
+            "What do you call a bike trail that shows no empathy? A cycle path",
+            "So the cops just came to my door, they said my dog was chasing someone on a bike, I told them “that's ridiculous, my dog doesn't even own a bike”",
+            "I gave my bike a new name after it came back from the bell repair shop. It has a nice ring to it.",
+            "Do you ValetLike out website? It's green. ", # based off of Asher's
+            "Green means go, so go ride a ValetBike. ", # why not
+            "I nearly ran over an old lady while on my bike yesterday. You need to learn to use a bell, she said. I know how to use a bell… I just can't ride my bike.",
+            "What will I fail at first? Riding a bike, or finals?", # why not
+            "My dog, Rover, used to chase everyone on a bike. I had to take his bike away.",
+            "Do you know what the hardest thing is about learning to ride a bike? The road.",
+            "Who's more likely to fail at riding a bike? Me or a child? The answer is me. I would cry.", # why not
+            "I tried riding a bike once. And then cried. ", # why not
+            "When my bike hurts me, I kick it back. You could say we're in a vicious cycle.",
+            "I rode my bike 10 miles to safely dispose of some paper, cans and bottles earlier. I was tired on the way back, I had to recycle.",
+            "I can't stand it when my bike keeps falling over.",
+            "I yelled “COW” at a woman on a bike. She flipped me off, then hit the cow.",
+            "Bought a stationary bike today, but I don't see myself getting anywhere with it.",
+            "Where do crazy people ride their bikes? On the psycho path.",
+            "Did you know that the longest a ValetBike was borrowed was 3 days! We're just that good and not because we never ended a ride.", # why not
+            "What did the tree do when the bank closed down? It started its own branch. ", # why not
+            "My friend got into a squabble with a driver for swerving into the bike lane. He's a bit of a cyclepath.",
+            "I know a bike mechanic who is a bit scary, I guess he has always been a crank-y dude.",
+            "Green is the color of envy, so go borrow a ValetBike so people will be envious of your bike. Not me though. Since I can't ride a bike.", # why not
+            "What does a biker do when he notices he's on the wrong trail? He back-pedals.",
+            "What's the perfect name for a sculptor who uses bike parts for his art. Cycleangelo.",
+            "Go touch grass. But you know what else has grass. GENSHIN IMPACT. (thi is to annoy Asher)", # why not
+            "A barber won a bike race, I asked him how? He said he took a short cut.",
+            "What name did the chemist give to the bike he built? Bike-carbonate of Soda.",
+            "What do you call a bike that loves learning? A uni-cycle.",
+            "My bike-mad cousin dropped out of university when he realized he signed up for psychology."
         ]
         puns.sample
     end
--- db/schema.rb
+++ db/schema.rb
@@ -20,6 +20,92 @@ ActiveRecord::Schema[7.0].define(version: 2022_12_07_202237) do
     t.integer "current_user_id"
   end
 
+  create_table "pay_charges", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
+    t.bigint "customer_id", null: false
+    t.bigint "subscription_id"
+    t.string "processor_id", null: false
+    t.integer "amount", null: false
+    t.string "currency"
+    t.integer "application_fee_amount"
+    t.integer "amount_refunded"
+    t.json "metadata"
+    t.json "data"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["customer_id", "processor_id"], name: "index_pay_charges_on_customer_id_and_processor_id", unique: true
+    t.index ["subscription_id"], name: "index_pay_charges_on_subscription_id"
+  end
+
+  create_table "pay_customers", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
+    t.string "owner_type"
+    t.bigint "owner_id"
+    t.string "processor", null: false
+    t.string "processor_id"
+    t.boolean "default"
+    t.json "data"
+    t.datetime "deleted_at", precision: nil
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["owner_type", "owner_id", "deleted_at", "default"], name: "pay_customer_owner_index"
+    t.index ["processor", "processor_id"], name: "index_pay_customers_on_processor_and_processor_id", unique: true
+  end
+
+  create_table "pay_merchants", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
+    t.string "owner_type"
+    t.bigint "owner_id"
+    t.string "processor", null: false
+    t.string "processor_id"
+    t.boolean "default"
+    t.json "data"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["owner_type", "owner_id", "processor"], name: "index_pay_merchants_on_owner_type_and_owner_id_and_processor"
+  end
+
+  create_table "pay_payment_methods", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
+    t.bigint "customer_id", null: false
+    t.string "processor_id", null: false
+    t.boolean "default"
+    t.string "type"
+    t.json "data"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["customer_id", "processor_id"], name: "index_pay_payment_methods_on_customer_id_and_processor_id", unique: true
+  end
+
+  create_table "pay_subscriptions", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
+    t.bigint "customer_id", null: false
+    t.string "name", null: false
+    t.string "processor_id", null: false
+    t.string "processor_plan", null: false
+    t.integer "quantity", default: 1, null: false
+    t.string "status", null: false
+    t.datetime "current_period_start", precision: nil
+    t.datetime "current_period_end", precision: nil
+    t.datetime "trial_ends_at", precision: nil
+    t.datetime "ends_at", precision: nil
+    t.boolean "metered"
+    t.string "pause_behavior"
+    t.datetime "pause_starts_at", precision: nil
+    t.datetime "pause_resumes_at", precision: nil
+    t.decimal "application_fee_percent", precision: 8, scale: 2
+    t.json "metadata"
+    t.json "data"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["customer_id", "processor_id"], name: "index_pay_subscriptions_on_customer_id_and_processor_id", unique: true
+    t.index ["metered"], name: "index_pay_subscriptions_on_metered"
+    t.index ["pause_starts_at"], name: "index_pay_subscriptions_on_pause_starts_at"
+  end
+
+  create_table "pay_webhooks", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
+    t.string "processor"
+    t.string "event_type"
+    t.json "event"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "rides", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
     t.integer "start_station_id"
     t.integer "end_station_id"
@@ -57,4 +143,8 @@ ActiveRecord::Schema[7.0].define(version: 2022_12_07_202237) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "pay_charges", "pay_customers", column: "customer_id"
+  add_foreign_key "pay_charges", "pay_subscriptions", column: "subscription_id"
+  add_foreign_key "pay_payment_methods", "pay_customers", column: "customer_id"
+  add_foreign_key "pay_subscriptions", "pay_customers", column: "customer_id"
 end
